e9265888aa843530418402e3b43b3d6a9022deca
joy-amorin <amorinkaren2021@gmail.com>
2024-05-23 20:18:20 -0300

    add templates and views

--- bookshelf/bookshelf_app/admin.py
+++ bookshelf/bookshelf_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, ReadingPlan
 
 # Register your models here.
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Book)
+admin.site.register(ReadingPlan)
