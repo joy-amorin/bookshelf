8c2aa8e70127ff4a3ca1887e2e99e38f189b500e
joy-amorin <amorinkaren2021@gmail.com>
2024-06-05 20:03:05 -0300

    add create_reading_plan and add_book functions

--- bookshelf/bookshelf_app/views.py
+++ bookshelf/bookshelf_app/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import BookForm, ReadingPlanForm 
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list') #redirect to the list books page after add a book
+    else:
+        form = BookForm()
+    return render(request, 'add_book.html', {'form': form})
+
+def create_reading_plan(request):
+    if request.method == 'POST':
+        form = ReadingPlanForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list') #redirect to the list books page after create a reading plan
+    else:
+        form = ReadingPlanForm()
+    return render(request, 'create_reading_plan.html', {'form': form})
