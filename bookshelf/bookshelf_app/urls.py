8ff921aea3dd3818cdbf3a1105c449663b676908
joy-amorin <amorinkaren2021@gmail.com>
2024-06-05 21:52:10 -0300

    add create_reading_plan and add_book functions and views

--- bookshelf/bookshelf_app/urls.py
+++ bookshelf/bookshelf_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [path("", views.index, name="index"),
-
+                path("add_book/", views.add_book, name='add_book'),# url to add a book
+                path("create_reading_plan/", views.create_reading_plan, name='create_reading_plan'), #url to create a reading plan
 ]
