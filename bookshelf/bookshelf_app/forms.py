afb2c130da1a1f6b7d3e9e5493b3e3855abf00a2
joy-amorin <amorinkaren2021@gmail.com>
2024-06-22 14:33:30 -0300

    add authors and genre as text

--- bookshelf/bookshelf_app/forms.py
+++ bookshelf/bookshelf_app/forms.py
@@ -1,10 +1,14 @@
 from django import forms
-from .models import Book, ReadingPlan
+from .models import Book, ReadingPlan, Author, Genre
 
 class BookForm(forms.ModelForm):
+
+    authot_name = forms.CharField(max_length=100, label="Author")
+    genre_name = forms.CharField(max_length=100, label="Genre")
     class Meta:
         model = Book
-        fields = ['title', 'author', 'genre', 'pages']
+        fields = ['title','pages']
+
 
 class ReadingPlanForm(forms.ModelForm):
     class Meta:
