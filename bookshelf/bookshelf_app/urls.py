f7518442f04bd720f10626f1e09be5da8aca8fe0
joy-amorin <amorinkaren2021@gmail.com>
2024-06-25 14:04:52 -0300

    fix booklist url

--- bookshelf/bookshelf_app/urls.py
+++ bookshelf/bookshelf_app/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [path("", views.index, name="index"),
                 path("add/", views.add_book, name='add_book'),# url to add a book
                 path("plan/", views.create_reading_plan, name='create_reading_plan'), #url to create a reading plan
-                path("booklist/", views.book_list, name='book_list.html'), #url to book list 
+                path("booklist/", views.book_list, name='book_list'), #url to book list 
 ]
