b5b4e30a32b28f6d1a9ce6158d45a999d9b718ca
joy-amorin <amorinkaren2021@gmail.com>
2024-07-30 17:55:13 -0300

    add forms and viwes to add and delete books

--- bookshelf/bookshelf_app/urls.py
+++ bookshelf/bookshelf_app/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.book_list, name='books_list'),
     path('book/<int:book_id>/', views.book_detail, name='book_detail'),
+    path('book/add/', views.add_book, name='add_book'),
+    path('book/<int:book_id>/delete/', views.delete_book, name='delete_book'),
 ]
--- bookshelf/bookshelf_app/views.py
+++ bookshelf/bookshelf_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import BookForm
 
 # Create your views here.
 from .models import Book
@@ -10,3 +11,21 @@ def book_list(request):
 def book_detail(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
     return render(request, 'bookshelf_app/book_detail.html', {'book': book})
+
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('books_list')
+    else:
+        form = BookForm()
+    return render(request, 'bookshelf_app/add_book.html', {'form': form})
+
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('books_list')
+    return render(request, 'bookshelf_app/confirm_delete.html', {'book': book})
+        
