1ab0c20c8c7dfbf2e40cca66d72a6b257557adc5
joy-amorin <amorinkaren2021@gmail.com>
2024-08-07 20:46:31 -0300

    add books from the aplication aand save them in the data base

--- bookshelf/bookshelf_app/forms.py
+++ bookshelf/bookshelf_app/forms.py
@@ -1,7 +1,17 @@
 from django import forms
-from .models import Book
+from .models import Book, Genre, Author
 
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ['title', 'author', 'genre', 'status']
+        fields = ['title','status']
+
+class AuthorForm(forms.ModelForm):
+    class Meta:
+        model = Author
+        fields =['name']
+
+class GenreForm(forms.ModelForm):
+    class Meta:
+        model = Genre
+        fields = ['name']
--- bookshelf/bookshelf_app/views.py
+++ bookshelf/bookshelf_app/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .forms import BookForm
-
+from .forms import BookForm, AuthorForm, GenreForm
 # Create your views here.
-from .models import Book
+from .models import Book, Author, Genre
 
 def book_list(request):
     books = Book.objects.all()
@@ -14,14 +13,36 @@ def book_detail(request, book_id):
 
 def add_book(request):
     if request.method == 'POST':
-        form = BookForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('books_list')
-    else:
-        form = BookForm()
-    return render(request, 'bookshelf_app/add_book.html', {'form': form})
+        book_form = BookForm(request.POST)
+        author_form = AuthorForm(request.POST)
+        genre_form = GenreForm(request.POST)
 
+        if book_form.is_valid() and author_form.is_valid() and genre_form.is_valid():
+            #save or create the author
+            author_name = author_form.cleaned_data['name']
+            author, created = Author.objects.get_or_create(name=author_name)
+            #save or create genre
+            genre_name = genre_form.cleaned_data['name']
+            genre, created = Genre.objects.get_or_create(name=genre_name)
+
+            #save the book with the author and genre
+            book = book_form.save(commit=False)
+            book.author = author
+            book.genre = genre
+            book.save()
+
+            return redirect('books_list')
+        
+    else:
+            book_form = BookForm()
+            author_form = AuthorForm()
+            genre_form = GenreForm()
+    return render(request, 'bookshelf_app/add_book.html', {
+            'book_form': book_form,
+            'author_form': author_form,
+            'genre_form': genre_form
+    })
+        
 def delete_book(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
     if request.method == 'POST':
